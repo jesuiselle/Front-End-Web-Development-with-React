1d7c43cd6f5f2243923ed057a8c3c523dca8bcd0
Oumayma Dhehibi <oumayma.dhehibi@esprit.tn>
2020-08-27 23:19:35 +0200

    Presentational and Container Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -13,7 +13,7 @@ class DishDetail extends Component{
                         <div>
                             <p>{comment.comment}</p>
                             <p>--{comment.author},
-                                {comment.date}</p>
+                                {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
                         </div>
                     </li>
 
@@ -31,26 +31,44 @@ class DishDetail extends Component{
         }
         else{
             return(
-                <div></div>
+                <div>
+                </div>
             )
+        }
+    }
+
+    renderDish(dish) {
+        if(dish != null) {
+            return (
+                <div>
+                    <Card>
+                        <CardImg width="100%" src={dish.image} alt={dish.name} />
+                        <CardBody>
+                            <CardTitle>{dish.name}</CardTitle>
+                            <CardText>{dish.description}</CardText>
+                        </CardBody>
+                    </Card>
+                </div>
+            )
+        }
+        else {
+            return (
+                <div></div>
+            );
         }
     }
 
     render() {
         return (
+            <div className="container">
             <div className="row">
                 <div className="col-12 col-md-5 m-1">
-                <Card>
-                    <CardImg width="100" src={this.props.dish.image} alt={this.props.dish.name} />
-                    <CardBody>
-                        <CardTitle>{this.props.dish.name}</CardTitle>
-                        <CardText>{this.props.dish.description}</CardText>
-                    </CardBody>
-                </Card>
+                    {this.renderDish(this.props.dish)}
                 </div>
                 <div className="col-12 col-md-5 m-1">
-                    {this.renderComments(this.props.dish.comments)}
+                    {this.props.dish && this.renderComments(this.props.dish.comments)}
                 </div>
+            </div>
             </div>
         );
     }
